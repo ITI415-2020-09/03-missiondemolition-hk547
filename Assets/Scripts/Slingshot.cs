8c1f25af2014fb41cf61b36b18c8e6d877131b03
hk547 <60270709+hk547@users.noreply.github.com>
2020-09-24 01:43:09 -0400

    Creating the Slingshot Class to Halo setActive
    Script

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launchPoint;
+
+    private void Awake()
+    {
+        //transform.Find("") searches for a child of slingshot named launchpoint and returns its transform
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        //setactive() launchpoint tells the game whether or not to ignore them
+    }
+
+    void OnMouseEnter()
+    {
+        //print("Slingshot: OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        //print("Slingshot: OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+}
